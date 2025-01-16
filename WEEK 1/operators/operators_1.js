45e09c564cdc31c8e980065ec295cfba984e0a26
Abdul734734 <s2500150@edu.bc.fi>
2025-01-16 14:54:51 +0200

    end of  lesson

---
+++ WEEK 1/operators/operators_1.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 6;
+
+let sum = a + b;
+console.log(sum);
+
+let subtract = a - b;
+console.log(subtract);
+
+let multiple = 5 * 6;
+console.log(multiple);
+
+let divide = 5 / 6;
+console.log(divide);
+
+let remainder = 5 % 6;
+console.log(remainder);
